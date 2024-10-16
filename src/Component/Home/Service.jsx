fd43cbac41957664a3a9b3916e2f5fa5ecb3b39f
Tariq Ali <tarikali7444@gmail.com>
2024-10-16 20:28:37 +0530

    changes done

--- src/Component/Home/Service.jsx
+++ src/Component/Home/Service.jsx
@@ -7,11 +7,11 @@ export const Service = () => {
       <h1 className="text-center text-blue-500 font-bold text-3xl">
         Curated Services Exclusively for you
       </h1>
-      <p className="text-center  w-full md:w-1/3 m-auto text-lg mt-4">
-        Our services is curated as per need of client and their business
+      <p className="text-center w-full md:w-1/3 m-auto text-lg mt-4">
+        Our services are curated as per the needs of clients and their business
         requirements
       </p>
-      <div className="my-10 md:my-20 grid  md:grid-cols-2  gap-x-20 gap-y-8  ">
+      <div className="my-10 md:my-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {ServiceData.map((item, index) => (
           <ServiceCard key={index} data={item} />
         ))}
--- src/Component/Home/Servicecard.jsx
+++ src/Component/Home/Servicecard.jsx
@@ -1,17 +1,17 @@
 export const ServiceCard = ({ data }) => {
   return (
-    <div className="bg-blue-100 shadow-lg p-4 cursor-pointer  rounded-lg  boxshadow md:flex  justify-between  ">
+    <div className="bg-blue-100 shadow-lg p-4 cursor-pointer rounded-lg boxshadow md:flex justify-between">
       <div className="md:w-1/2">
-        <h3 className="font-semibold text-xl mb-2 text-black-500">
+        <h3 className="font-semibold text-xl mb-2 text-black">
           {data?.title}
         </h3>
         <p className="text-black-400 leading-5">{data?.description}</p>
       </div>
-      <div className="md:w-1/2 h-28 md:h-52 md:flex align-bottom   ">
+      <div className="md:w-1/2 h-28 md:h-52 md:flex items-center justify-center">
         <img
-          className="w-full h-full  rounded-md bg-transparent "
+          className="w-full h-full object-cover rounded-md bg-transparent"
           src={data?.img}
-          alt="Service"
+          alt={data?.title}
         />
       </div>
     </div>
